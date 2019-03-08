62ecd58080f5d79510238005049b41c57790a4a8
NSRdatabase <47895471+NSRdatabase@users.noreply.github.com>
2019-03-08 12:40:31 -0800

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -134,7 +134,8 @@ $(window).on('load', function() {
         var marker = L.marker([point.Latitude, point.Longitude], {icon: icon})
           .bindPopup("<b>" + point['Name'] + '</b><br>' +
           (point['Image'] ? ('<img src="' + point['Image'] + '"><br>') : '') +
-          point['Description'] + point['Notes']);
+          "Description: " + point['Description'] + '<br>' +
+          "Notes: " + point['Notes']);
 
         if (layers !== undefined && layers.length !== 1) {
           marker.addTo(layers[point.Group]);
